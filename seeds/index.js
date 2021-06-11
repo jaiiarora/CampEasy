56a4da3650773419fe7ed23c35cdabe1a13b24f9
Jai Arora <39581119+jaiiarora@users.noreply.github.com>
2021-06-12 03:29:33 +0530

    Database Seeding Completed

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const path=require('path');
 //setting up ejs engine
 const Campground=require('../models/campground');
 const cities=require("./cities");
+const {places,descriptors}=require("./seedHelpers");
 mongoose.connect("mongodb://localhost:27017/camp-easy",{
     useNewUrlParser:true,
     useCreateIndex: true,
@@ -15,6 +16,8 @@ db.once("open",()=>{
     console.log("Database Connected");
 });
 
+const sample=(array) => array[Math.floor(Math.random()*array.length)];
+
 const seedDB= async ()=>{
     await Campground.deleteMany({});
     for (let i=0; i<50;i++)
@@ -23,8 +26,11 @@ const seedDB= async ()=>{
         console.log(cities[random1000].city);
         //mark the backticks here
         const randCamp = new Campground({
-        location: `${cities[random1000].city}, ${cities[random1000].state}`});
+        location: `${cities[random1000].city}, ${cities[random1000].state}`, title:`${sample(descriptors)} ${sample(places)}`});
+
         await (randCamp.save());
     }
 }
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
